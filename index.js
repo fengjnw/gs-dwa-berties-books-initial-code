c8992a7a906fa63579a526330f33db3227d98f8b
fengjnw <fengjnw@gmail.com>
2025-11-20 13:24:42 +0000

    Fix: update environment variable names in .env, README, and index.js for consistency

--- index.js
+++ index.js
@@ -27,10 +27,10 @@ app.locals.shopData = { shopName: "Bertie's Books" }
 
 // 
 const db = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    host: process.env.BB_HOST,
+    user: process.env.BB_USER,
+    password: process.env.BB_PASSWORD,
+    database: process.env.BB_DATABASE,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
